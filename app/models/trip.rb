6f17eee39af893345b37de81c65a5bec4c018a3d
matias jara <mjara@MacBook-Pro-de-matias.local>
2016-12-20 17:12:59 -0300

    cambio trip

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -8,11 +8,11 @@ class Trip < ActiveRecord::Base
 validates_presence_of :salida, :estimada, :motivo, :ship_id, :purchase_id, message: "no puede estar en blanco"
 validates_uniqueness_of :purchase_id, message: "esta en uso"
 
-  validates_each :salida, :estimada do |model, attr, value|
-    if value.to_datetime < 1.day.ago.to_s
-      model.errors.add(attr, "la fecha #{value} no puede ser menor a la actual")
-    end
-  end
+  #validates_each :salida, :estimada do |model, attr, value|
+  #  if value.to_datetime < 1.day.ago.to_s
+  #    model.errors.add(attr, "la fecha #{value} no puede ser menor a la actual")
+  #  end
+  #end
   #validates_each :estimada do |model, attr, value|
   #  if value.to_datetime <= :salida.value
   #    model.errors.add(attr, "la fecha #{value} no puede ser menor que la salida")
