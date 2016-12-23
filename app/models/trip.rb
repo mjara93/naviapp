936b9678b8c821bbcb5a6fe9e862139c48b999f8
matias jara <mjara@MacBook-Pro-de-matias.local>
2016-12-22 23:28:28 -0300

    ultimo deploy

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -8,6 +8,8 @@ class Trip < ActiveRecord::Base
 validates_presence_of :salida, :estimada, :motivo, :ship_id, :purchase_id, message: "no puede estar en blanco"
 validates_uniqueness_of :purchase_id, message: "esta en uso"
 
+validate :validates_fecha
+validate :validate_estimada
   #validates_each :salida, :estimada do |model, attr, value|
   #  if value.to_datetime < 1.day.ago.to_s
   #    model.errors.add(attr, "la fecha #{value} no puede ser menor a la actual")
@@ -18,5 +20,15 @@ validates_uniqueness_of :purchase_id, message: "esta en uso"
   #    model.errors.add(attr, "la fecha #{value} no puede ser menor que la salida")
   #  end
   #end
+def validates_fecha
+  if salida.to_datetime < Date.current.to_s
+    errors.add(:message, "la fecha #{salida} no puede ser menor a la fecha actual")
+  end
+end
 
+def validate_estimada
+  if estimada.to_datetime <= salida.to_datetime
+    errors.add(:message, "la fecha estimada no puede ser menor o igual a la fecha de salida")
+  end
+end
 end
